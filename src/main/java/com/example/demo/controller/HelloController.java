09e227d7254b890dec64918fe14db3970be0bcb4
duanhao <duanhao@bgi.com>
2024-04-23 16:41:29 +0800

    【springboot】DispatcherServlet原理描述

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,18 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @see org.springframework.web.servlet.DispatcherServlet
+ * <p>
+ * Handler 对应Controller里面的方法，所有@RequestMapping方法都可以被看作是一个Handler；
+ * HandlerMapping  存的是请求路径与Handler对应关系
+ * HandlerAdapter  适配器，用于执行Handler
+ * <p>
+ * ·根据请求找到Handler；
+ * ·根据Handler找到对应的HandlerAdapter
+ * ·HandlerAdapter处理Handler
+ * ·处理结果
+ */
 @RestController
 public class HelloController {
 
