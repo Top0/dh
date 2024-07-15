21ad88cc1d29cb16d0bb8856c7046230b2a334d8
duanhao <duanhao@bgi.com>
2024-07-15 14:53:08 +0800

    【springboot-redisson】 redisson配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://github.com/redisson/redisson/wiki/1.-Overview
+ */
 @SpringBootApplication
 public class DemoApplication {
 
