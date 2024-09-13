ec84f5835b9f4e3365dde5698ad768fba9de67d3
duanhao <duanhao@bgi.com>
2024-09-13 10:23:20 +0800

    【springboot-mysql-mybatis-plus】 baomidou初始化

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +16,14 @@ class DemoApplicationTests {
     void contextLoads() {
     }
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.isTrue(5 == userList.size(), "");
+        userList.forEach(System.out::println);
+    }
 }
