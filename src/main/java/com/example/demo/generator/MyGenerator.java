1559898f03756c1b09133eae3e9c83279ab49cf5
duanhao <duanhao@bgi.com>
2024-09-13 10:55:47 +0800

    【springboot-mysql-mybatis-plus】代码生成器

---
+++ src/main/java/com/example/demo/generator/MyGenerator.java
@@ -0,0 +1,46 @@
+package com.example.demo.generator;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.nio.file.Paths;
+import java.sql.Types;
+
+public class MyGenerator {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("url",
+                        "name", "pwd")
+                .globalConfig(builder -> {
+                    builder.author("xxxx") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java"); // 指定输出目录
+                })
+                .dataSourceConfig(builder ->
+                        builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+                            int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+                            if (typeCode == Types.SMALLINT) {
+                                // 自定义类型转换
+                                return DbColumnType.INTEGER;
+                            }
+                            return typeRegistry.getColumnType(metaInfo);
+                        })
+                )
+                .packageConfig(builder -> builder
+                        .parent("com.example.demo")
+                        .entity("entity")
+                        .mapper("mapper")
+                        .service("service")
+                        .serviceImpl("service.impl")
+                        .xml("mapper.xml")
+                )
+                .strategyConfig(builder ->
+                        builder.addInclude("user") // 设置需要生成的表名
+                                .addTablePrefix("t_", "c_") // 设置过滤表前缀
+                                .entityBuilder()
+                                .enableLombok()
+                )
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
