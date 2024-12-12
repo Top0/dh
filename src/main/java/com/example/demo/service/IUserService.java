481c7df2b6498c9d8fd43822b01b6dc2d5769adf
duanhao <duanhao@bgi.com>
2024-12-12 10:49:31 +0800

    sql

--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -1,16 +1,18 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.demo.entity.User;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author baomidou
  * @since 2024-09-13
  */
 public interface IUserService extends IService<User> {
-
+    List<User> queryAdultUser();
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,14 +1,18 @@
 package com.example.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author baomidou
@@ -16,5 +20,11 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Resource
+    UserMapper userMapper;
 
+    @Override
+    public List<User> queryAdultUser() {
+        return userMapper.selectList(Wrappers.<User>lambdaQuery().gt(User::getAge, 18));
+    }
 }
